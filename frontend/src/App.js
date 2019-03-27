707314248c4b1fd1ee918d82943321c4e28e9691
lamdang98 <ltdang16@earlham.edu>
2019-03-26 20:38:16 -0400

    Redesign Header and block-like layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,8 +17,8 @@ class App extends Component {
 			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"/>
 			<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 			<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-		</head>
-		
+			<link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'/>
+			</head>		
 		<body>
 			<div class="container-fluid"> 
 				<Header/>
