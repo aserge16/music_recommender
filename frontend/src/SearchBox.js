0ada0fc78cebf61bfcd4d42c31c562d8338993ff
lamdang98 <ltdang16@earlham.edu>
2019-03-27 21:35:15 -0400

    Fix layout

--- frontend/src/SearchBox.js
+++ frontend/src/SearchBox.js
@@ -6,23 +6,9 @@ class SearchBox extends Component{
 	
 	render() {
 		return(
-			<div class='row search-box'>
-				<div class="col-md-8 pr-md-1">
-					<form method='get' action=''>
-						<div class='tb'>
-							<div class='td'>
-								<input type='text' placeholder='Search' required/>
-							</div>
-							<div class='td' id='s-cover'>
-								<button type='submit'>
-									<div id="s-circle"></div>
-									<span></span>
-								</button>
-							</div>
-						</div>
-					</form>
-				</div>
-				<div class="col-md-4 pl-md-2 check-box">Checkbox</div>
+			<div class='row'>
+				<div class="column search-box">search</div>
+				<div class="column check-box">Checkbox</div>
 			</div>
 		)
 	}
