ac575bc87b614e7448737fb45ca180eb046822ca
lamdang98 <ltdang16@earlham.edu>
2019-03-27 23:46:47 -0400

    partial implementation of search bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,6 +15,7 @@ class App extends Component {
 			<meta charset="utf-8"/>
 			<meta name="viewport" content="width=device-width, initial-scale=1"/>
 			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"/>
+			<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous"/>
 			<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 			<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
 			<link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'/>
--- frontend/src/SearchBox.js
+++ frontend/src/SearchBox.js
@@ -7,8 +7,15 @@ class SearchBox extends Component{
 	render() {
 		return(
 			<div class='row'>
-				<div class="column search-box">search</div>
-				<div class="column check-box">Checkbox</div>
+				<div class="column search-col">
+					<div class="search-box">
+						<input type="text" name="search" placeholder="Add songs to checklist" class="search-txt"/>
+						<a class="search-btn" href='#'>
+							<i class="fas fa-search"></i>
+						</a>
+					</div>
+				</div>
+				<div class="column check-col">Checkbox</div>
 			</div>
 		)
 	}
