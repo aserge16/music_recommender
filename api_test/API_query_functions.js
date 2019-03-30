6226ef5322511ff032553908437191aa7ca7658b
aserge16 <aleks.sergeev2@gmail.com>
2019-03-30 12:41:51 -0400

    Axios query now working

--- api_test/API_query_functions.js
+++ api_test/API_query_functions.js
@@ -1,26 +1,38 @@
-const axios = require('axios')
+var axios = require("axios");
+var CLIENT_ID = "7cc2582a05fa493f8ae45727393829fe";
+var SECRET_TOKEN = "ee5dab7ff4ac48a288d22017b558e9ae";
 
-const getAccessToken = async () => {
-    try {
-        return await axios.post('https://accounts.spotify.com/api/token', {
-            headers: {
-                'Authorization': 'Basic N2NjMjU4MmEwNWZhNDkzZjhhZTQ1NzI3MzkzODI5ZmU6ZWU1ZGFiN2ZmNGFjNDhhMjg4ZDIyMDE3YjU1OGU5YWU=',
-            },
-            data: {
-                'grant_type': 'client_credentials'
-            }
-        })
-    } catch (error) {
-        console.error(error)
+var encoded_token = Buffer.from(CLIENT_ID + ":" + SECRET_TOKEN)
+    .toString('base64');    // encode to base64
+
+
+// Get the access token
+axios.post("https://accounts.spotify.com/api/token",
+    "grant_type=client_credentials", {
+        headers: {
+            Authorization: "Basic " + encoded_token,
+            "Content-Type": "application/x-www-form-urlencoded",
+            "grant_type": "client_credentials"
+        }
     }
-}
+).then(function (response) {
+    // search for artists named "queen"
+    access_token = response.data.access_token;
 
-const reportToken = async () => {
-    const accessToken = await getAccessToken()
-
-    if (accessToken) {
-        console.log(`Your token is ${accessToken}`)
-    }
-}
-
-reportToken()
+    axios.get('https://api.spotify.com/v1/search', {
+        headers: {
+            Authorization: "Bearer " + access_token
+        },
+        params: {
+            q: "queen",
+            type: "artist"
+        }
+    }).then(function (res) {
+        console.log(res.data.artists.items);
+    }).catch(function (error) {
+    console.log(error);
+    })
+}).catch(function(error) {
+    console.log(error.response.data);
+    console.log(error.response.status);
+});
