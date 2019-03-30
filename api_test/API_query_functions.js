82a178dc3804b0dc8675d573f48836447c406d6c
aserge16 <aleks.sergeev2@gmail.com>
2019-03-30 14:15:01 -0400

    Created query function for artist search

--- api_test/API_query_functions.js
+++ api_test/API_query_functions.js
@@ -6,33 +6,46 @@ var encoded_token = Buffer.from(CLIENT_ID + ":" + SECRET_TOKEN)
     .toString('base64');    // encode to base64
 
 
-// Get the access token
-axios.post("https://accounts.spotify.com/api/token",
-    "grant_type=client_credentials", {
-        headers: {
-            Authorization: "Basic " + encoded_token,
-            "Content-Type": "application/x-www-form-urlencoded",
-            "grant_type": "client_credentials"
-        }
-    }
-).then(function (response) {
-    // search for artists named "queen"
-    access_token = response.data.access_token;
 
-    axios.get('https://api.spotify.com/v1/search', {
-        headers: {
-            Authorization: "Bearer " + access_token
-        },
-        params: {
-            q: "queen",
-            type: "artist"
-        }
-    }).then(function (res) {
-        console.log(res.data.artists.items);
-    }).catch(function (error) {
-    console.log(error);
-    })
-}).catch(function(error) {
-    console.log(error.response.data);
-    console.log(error.response.status);
-});
+const getAccessToken = async () => {
+    try {
+        response = await axios.post("https://accounts.spotify.com/api/token",
+            "grant_type=client_credentials", {
+                headers: {
+                    Authorization: "Basic " + encoded_token,
+                    "Content-Type": "application/x-www-form-urlencoded",
+                    "grant_type": "client_credentials"
+                }
+            });
+        ACCESS_TOKEN = response.data.access_token;
+        console.log(ACCESS_TOKEN);
+        return ACCESS_TOKEN;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
+async function searchArtists(searchItem) {
+    const ACCESS_TOKEN = await getAccessToken();
+    try {
+        axios.get('https://api.spotify.com/v1/search', {
+            headers: {
+                Authorization: "Bearer " + ACCESS_TOKEN
+            },
+            params: {
+                q: searchItem,
+                type: "artist"
+            }
+        }).then(function (res) {
+            console.log(res.data.artists.items);
+        }).catch(function (error) {
+            console.log(error);
+        })
+    } catch (error) {
+        console.log(error.response.data);
+        console.log(error.response.status);
+    }
+}
+
+searchArtists("queen")
