a30bdc3676acc44a65697ab17bcac90a0a754f9b
aserge16 <aleks.sergeev2@gmail.com>
2019-04-08 20:17:42 -0500

    Functions return objects with wanted data now

--- api_test/API_query_functions.js
+++ api_test/API_query_functions.js
@@ -1,11 +1,9 @@
 var axios = require("axios");
 var CLIENT_ID = "7cc2582a05fa493f8ae45727393829fe";
 var SECRET_TOKEN = "ee5dab7ff4ac48a288d22017b558e9ae";
-var ACCESS_TOKEN = "";
 
 var encoded_token = Buffer.from(CLIENT_ID + ":" + SECRET_TOKEN)
     .toString('base64');    // encode to base64
-
 
 
 const getAccessToken = async () => {
@@ -18,7 +16,6 @@ const getAccessToken = async () => {
                 }
             });
         ACCESS_TOKEN = response.data.access_token;
-        console.log(ACCESS_TOKEN);
         return ACCESS_TOKEN;
     } catch (error) {
         console.error(error);
@@ -35,10 +32,17 @@ async function searchArtists(searchItem) {
             },
             params: {
                 q: searchItem,
-                type: "artist"
+                type: "artist",
+                limit: 15
             }
         }).then(function (res) {
-            console.log(res.data.artists.items);
+            var artists = [];
+            for (var i = 0; i < res.data.artists.items.length; i++) {
+                var item = res.data.artists.items[i];
+                var artist = {name:item.name, id:item.id, image_url:item.images[0].url};
+                artists[i] = artist;
+            }
+            return artists;
         }).catch(function (error) {
             console.log(error);
         })
@@ -58,10 +62,21 @@ async function searchTracks(searchItem) {
             },
             params: {
                 q: searchItem,
-                type: "track"
+                type: "track",
+                limit: 15
             }
         }).then(function (res) {
-            console.log(res.data.tracks.items);
+            var tracks = [];
+            for (var i = 0; i < res.data.tracks.items.length; i++) {
+                var item = res.data.tracks.items[i];
+                var artists = [];
+                for (var j = 0; j < item.artists.length; j++) {
+                    artists[j] = item.artists[j].name;
+                }
+                var track = {name:item.name, id:item.id, artists:artists, image_url:item.album.images[0].url};
+                tracks[i] = track;
+            }
+            return tracks;
         }).catch(function (error) {
             console.log(error);
         })
@@ -72,7 +87,7 @@ async function searchTracks(searchItem) {
 }
 
 
-async function getRecommendations(seeds, callback) {
+async function getRecommendations(seeds) {
     const ACCESS_TOKEN = await getAccessToken();
     try {
         axios.get('https://api.spotify.com/v1/recommendations', {
@@ -82,11 +97,21 @@ async function getRecommendations(seeds, callback) {
             params: {
                 seed_tracks: songs,
                 seed_artists: artists,
-                seed_genres: genres
+                seed_genres: genres,
+                limit: 15
             }
         }).then(function (res) {
-            callback(res.data);
-            console.log(res.data);
+            var tracks = [];
+            for (var i = 0; i < res.data.tracks.length; i++) {
+                var item = res.data.tracks[i];
+                var artists = [];
+                for (var j = 0; j < item.artists.length; j++) {
+                    artists[j] = item.artists[j].name;
+                }
+                var track = {name:item.name, id:item.id, artists:artists, image_url:item.album.images[0].url};
+                tracks[i] = track;
+            }
+            return tracks;
         }).catch(function (error) {
             console.log(error);
         })
@@ -99,5 +124,7 @@ async function getRecommendations(seeds, callback) {
 //searchTracks("shotgun knees")
 songs = '5xhFyuXigbt6RAJR7k2aDs,1TKYPzH66GwsqyJFKFkBHQ';
 artists = '5xhFyuXigbt6RAJR7k2aDs';
-getRecommendations(songs, artists, [], (res) => console.log(res));
+genres = "";
+seed = {songs:songs, artists:artists, genres:genres};
+getRecommendations(seed)
 
