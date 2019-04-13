d40f8402d2962cb8356855a47aec08435f6cef11
lamdang98 <ltdang16@earlham.edu>
2019-04-12 23:16:23 -0400

    AutoSuggestion Box init

--- frontend/src/AutoSuggestionBox.js
+++ frontend/src/AutoSuggestionBox.js
@@ -8,7 +8,17 @@ class AutoSuggestionBox extends Component{
     render() {
 		return (
 			<div>
-				
+				<ul class="list-group">
+				<li class="list-group-item">
+					<img src='https://img.kpopmap.com/2018/10/izone-blood-type-sakura-cover.jpg'/>
+				<p> Airplane - Minayawa Sakura </p>
+					
+				</li>
+				<li class="list-group-item">Dapibus ac facilisis in</li>
+				<li class="list-group-item">Morbi leo risus</li>
+				<li class="list-group-item">Porta ac consectetur ac</li>
+				<li class="list-group-item">Vestibulum at eros</li>
+				</ul>
 			</div>
 		);
 	}
