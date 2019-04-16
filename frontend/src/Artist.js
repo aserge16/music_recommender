0e2ed49d591b15ba0471d106a4f93765e15aba4f
Anh Dang <aqdang16@earlham.edu>
2019-04-15 20:32:13 -0400

    fix warnings
    remember to use 'className' instead of 'class'

--- frontend/src/Artist.js
+++ frontend/src/Artist.js
@@ -5,24 +5,23 @@ import './style.css';
 class Artist extends Component{
 	render() {
 		return (
-			<div class="live_scroll">
-				<div class="row new-row song-component">
-                        {
-                            this.props.artists.map((artist) => {
-                                return (
-                                    <div class='artist-box'>
-                                        <ArtistView
-                                            artistID={artist.id}
-                                        />
-                                        <img src={artist.image_url}/>
-                                        <p> {artist.name} </p>
-                                    </div>
-                                );
-                            })
-                        }
-                    </div>
-                </div>
-			// </div>
+			<div className="live_scroll">
+				<div className="row new-row song-component">
+					{
+						this.props.artists.map((artist) => 
+							//return (
+								<div className='artist-box' key={artist.id}>
+									<ArtistView
+										artistID={artist.id}
+									/>
+									<img src={artist.image_url}/>
+									<p> {artist.name} </p>
+								</div>
+							//);
+						)
+					}
+				</div>
+			</div>
 		);
 	}
 }
