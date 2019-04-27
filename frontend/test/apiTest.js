e1d1127c9581dff93edc9b2c81d33f31b6947cde
Vuong Khuat <vuongkhuat0407@gmail.com>
2019-04-27 14:18:19 -0400

    add unit test files

---
+++ frontend/test/apiTest.js
@@ -0,0 +1,8 @@
+const assert = require("assert");
+const API = require("../src/API_query_functions");
+
+describe('test func', function() {
+    it('1 is 1', function() {
+        API.searchTracks("");
+    });
+});
