43ddad77b4c69ac2d75a20cbca501c1fee7fe061
Vuong Khuat <vuongkhuat0407@gmail.com>
2019-04-28 21:14:49 -0400

    add unit tests

---
+++ frontend/API_for_test.js
@@ -0,0 +1,165 @@
+var axios = require("axios");
+var genres = require("./src/genre_seeds");
+
+async function searchArtists(searchItem, token, callback) {
+    axios.get('https://api.spotify.com/v1/search', {
+        headers: {
+            Authorization: "Bearer " + token
+        },
+        params: {
+            q: searchItem,
+            type: "artist",
+            limit: 15
+        }
+    }).then(function (res) {
+        var artists = [];
+        for (var i = 0; i < res.data.artists.items.length; i++) {
+            var item = res.data.artists.items[i];
+            if (item.images.length !== 0) {
+                var artist = {name:item.name, id:item.id, image_url:item.images[0].url};
+            } else {
+                artist = {name:item.name, id:item.id, image_url:""}
+            }
+            artists[i] = artist;
+        }
+        callback(artists);
+    }).catch(function (error) {
+        console.log(error);
+    })
+}
+
+async function searchTracks(searchItem, token, callback) {
+    axios.get('https://api.spotify.com/v1/search', {
+        headers: {
+            Authorization: "Bearer " + token
+        },
+        params: {
+            q: searchItem,
+            type: "track",
+            limit: 7
+        }
+    }).then(function (res) {
+        var tracks = [];
+        for (var i = 0; i < res.data.tracks.items.length; i++) {
+            var item = res.data.tracks.items[i];
+            var artists = [];
+            for (var j = 0; j < item.artists.length; j++) {
+                artists[j] = item.artists[j].name;
+            }
+            var track = {name:item.name, id:item.id, artists:artists, image_url:item.album.images[0].url};
+            tracks[i] = track;
+        }
+        callback(tracks);
+    }).catch(function (error) {
+        console.log(error);
+    })
+}
+
+async function searchGenres(searchItem, callback) {
+    searchItem = searchItem.toLowerCase();
+    var results = genres.genres.filter((genre) => genre.search(searchItem) !== -1)
+    callback(results)
+}
+
+
+async function getRecommendations(seed, token, callback) {
+    var tracks = '';
+    if (seed.songs !== undefined) {
+        tracks = seed.songs.map((song) => song.id).join()
+    }
+
+    var artists = '';
+    if (seed.artists !== undefined) {
+        artists = seed.artists.map((artist) => artist.id).join()
+    }
+
+    if (seed.genres !== undefined) {
+        genres = seed.genres.join()
+    }
+
+    try {
+        axios.get('https://api.spotify.com/v1/recommendations', {
+            headers: {
+                Authorization: "Bearer " + token
+            },
+            params: {
+                seed_tracks: tracks,
+                seed_artists: artists,
+                seed_genres: genres,
+                limit: 15
+            }
+        }).then(function (res) {
+            var tracks = [];
+            for (var i = 0; i < res.data.tracks.length; i++) {
+                var item = res.data.tracks[i];
+                var artists = item.artists.map((artist) => artist.name)
+
+                var track = {
+                    name: item.name, 
+                    id: item.id, 
+                    artists: artists, 
+                    image_url: item.album.images[0].url
+                };
+                tracks.push(track);
+            }
+            callback(tracks);
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function getRelatedArtists(artists, token, callback) {
+    // Given a list of artists id, find all their related artists and combine them
+    // into a single list. Then sort the list by the artist's popularity
+    // and return the first 15 results.
+    var allRequests = artists.map((id) => {
+        return axios.get(`https://api.spotify.com/v1/artists/${id}/related-artists`,
+            {
+                headers: {
+                Authorization: "Bearer " + token
+                }
+            }
+    )});
+
+    axios.all(allRequests).then(function (res) {
+        // combine results, merge into a single list
+        var allRelatedArtists = res.map(result => result.data.artists)
+        allRelatedArtists = allRelatedArtists.flat(1)
+
+        // sort the list based on popularity, take first 15 results
+        allRelatedArtists.sort((a, b) => b.popularity - a.popularity)
+        var toReturn = allRelatedArtists.slice(0, 15)
+        callback(toReturn)
+        
+    }).catch(function (error) {
+        console.log(error);
+    })
+}
+
+
+async function getCategory(categories, token, callback) {
+    var allRequests = categories.map((category) => {
+        return axios.get(`https://api.spotify.com/v1/browse/categories/${category}/playlists`,
+            {
+                headers: {
+                Authorization: "Bearer " + token
+                }
+            }
+    )});
+
+    axios.all(allRequests).then(function (res) {
+        var allPlaylists = res.map(result => result.data.playlists.items)
+        allPlaylists = allPlaylists.flat(1)
+        callback(allPlaylists)
+    }).catch(function (error) {
+        console.log(error);
+    })
+}
+
+module.exports.searchArtists = searchArtists;
+module.exports.searchTracks = searchTracks;
+module.exports.searchGenres = searchGenres;
+module.exports.getRecommendations = getRecommendations;
+module.exports.getRelatedArtists = getRelatedArtists;
+module.exports.getCategory = getCategory;
--- frontend/test/apiTest.js
+++ frontend/test/apiTest.js
@@ -1,67 +1,102 @@
-// import assert from "assert";
-const API = require("../src/API_query_functions");
+const assert = require("assert");
+const API = require("../API_for_test");
 
-// const token = "";
+const token = "BQCLJz7qHggiIXK0-Ttdxa1KzfRc1x3aVpCsZxSg_OGjvradpCvvc3qorlbUDng0wux3B-nQjecOp_t-F2c";
 
-// describe('searchArtists results have the right format', function() {
-//     it('1 is 1', function(done) {
-//         API.searchTracks("under pressure", token, done)
-//             .then((artists) => {
-//                 artists.forEach((artist) => {
-//                     assert.typeOf(artist.name, "string");
-//                     assert.typeOf(artist.id, "string");
-//                     assert.typeOf(artist.image_url, "string");
-//                 });
+describe('searchArtists', function() {
+    it('searchArtists results have the right format', function() {
+        API.searchArtists("david bowie", token, (artists) => {
+            artists.forEach((artist) => {
+                assert.deepEqual(Object.keys(artist), ["name", "id", "image_url"]);
+                assert.equal(typeof artist.name, "string");
+                assert.equal(typeof artist.id, "string");
+                assert.equal(typeof artist.image_url, "string");
+            });
+        })
+    });
 
-//                 done(artists);
-//             });
-//     });
-// });
+    it('searchArtists with no results returns empty list', function() {
+        // create a random search query with no results
+        searchItem = "aidfhawkefawefawefnwaelflshgvaowh";
 
-// describe('searchArtists with no results returns empty list', function() {
-//     it('1 is 1', function() {
-//         API.searchTracks("");
-//     });
-// });
+        API.searchArtists(searchItem, token, (artists) => {
+            assert.equal(artists.length, 0);
+        })
+    });
+});
 
-// describe('searchTracks results have the right format', function() {
-//     it('1 is 1', function() {
-//         API.searchTracks("");
-//     });
-// });
+describe('searchTracks', function() {
+    it('searchTracks results have the right format', function() {
+        API.searchTracks("under pressure", token, (tracks) => {
+            tracks.forEach((track) => {
+                assert.deepEqual(Object.keys(track), ["name", "id", "artists", "image_url"]);
+                assert.equal(typeof track.name, "string");
+                assert.equal(typeof track.id, "string");
+                assert.equal(typeof track.artists, "object");
+                assert.equal(typeof track.artists[0], "string");
+                assert.equal(typeof track.image_url, "string");
+            });
+        });
+    });
 
-// describe('searchTracks with no results returns empty list', function() {
-//     it('1 is 1', function() {
-//         API.searchTracks("");
-//     });
-// });
+    it('searchTracks with no results returns empty list', function() {
+        // create a random search query with no results
+        searchItem = "aidfhawkefawefawefnwaelflshgvaowh";
 
-// describe('searchGenres results have the right format', function() {
-//     it('1 is 1', function() {
-//         API.searchTracks("");
-//     });
-// });
+        API.searchTracks(searchItem, token, (tracks) => {
+            assert.equal(tracks.length, 0);
+        })
+    });
+});
 
-// describe('searchGenres with no results returns empty list', function() {
-//     it('1 is 1', function() {
-//         API.searchTracks("");
-//     });
-// });
+describe('searchGenres', function() {
+    it('searchGenres results have the right format', function() {
+        API.searchGenres("rock", (genres) => {
+            genres.forEach((genre) => {
+                assert.equal(typeof genre, "string");
+            })
+        });
+    });
 
-// describe('getRecommendations results have the right format', function() {
-//     it('1 is 1', function() {
-//         API.searchTracks("");
-//     });
-// });
+    it('searchGenres with no results returns empty list', function() {
+        // create a random search query with no results
+        searchItem = "aidfhawkefawefawefnwaelflshgvaowh";
 
-// describe('getRelatedArtists results have the right format', function() {
-//     it('1 is 1', function() {
-//         API.searchTracks("");
-//     });
-// });
+        API.searchGenres(searchItem, (genres) => {
+            assert.equal(genres.length, 0);
+        })
+    });
+});
 
-// describe('getCategory results have the right format', function() {
-//     it('1 is 1', function() {
-//         API.searchTracks("");
-//     });
-// });
+describe('getRecommendations', function() {
+    it('getRecommendations results have the right format', function() {
+        seed = {
+            artist: [
+                {
+                    id: "0oSGxfWSnnOXhD2fKuz2Gy",
+                    image_url: "https://i.scdn.co/image/6efa04809a6358a5da0e701d24449cfb8348ebf5",
+                    name: "David Bowie"
+                }
+            ],
+            songs: [
+                {
+                    artists: ["Queen", "David Bowie"],
+                    id: "11IzgLRXV7Cgek3tEgGgjw",
+                    image_url: "https://i.scdn.co/image/140a81a2d5930b281f6ce32d2e673461c5473718",
+                    name: "Under Pressure - Remastered"
+                }
+            ],
+            genres: ["j-rock"]
+        };
+        API.getRecommendations(seed, token, (recs) => {
+            assert.equal(typeof recs, "object");
+            recs.forEach((rec) => {
+                assert.equal(typeof rec.name, "string");
+                assert.equal(typeof rec.id, "string");
+                assert.equal(typeof rec.artists, "object");
+                assert.equal(typeof rec.artists[0], "string");
+                assert.equal(typeof rec.image_url, "string");
+            })
+        });
+    });
+});
